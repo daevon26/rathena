f7fc5ee73230288bf53bae16afa284d5d707b8eb
daevon26 <daevon26@gmail.com>
2017-03-23 10:26:33 +0800

    Add files via upload

---
+++ sql-files/item_db2_re.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u8
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 23, 2017 at 02:14 AM
+-- Server version: 5.5.54
+-- PHP Version: 5.4.45-0+deb7u7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `imperialro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_db2_re`
+--
+
+CREATE TABLE IF NOT EXISTS `item_db2_re` (
+  `id` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `name_english` varchar(50) NOT NULL DEFAULT '',
+  `name_japanese` varchar(50) NOT NULL DEFAULT '',
+  `type` tinyint(2) unsigned NOT NULL DEFAULT '0',
+  `price_buy` mediumint(8) unsigned DEFAULT NULL,
+  `price_sell` mediumint(8) unsigned DEFAULT NULL,
+  `weight` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `attack` smallint(5) unsigned DEFAULT NULL,
+  `defence` smallint(5) unsigned DEFAULT NULL,
+  `range` tinyint(2) unsigned DEFAULT NULL,
+  `slots` tinyint(2) unsigned DEFAULT NULL,
+  `equip_jobs` bigint(20) unsigned DEFAULT NULL,
+  `equip_upper` tinyint(2) unsigned DEFAULT NULL,
+  `equip_genders` tinyint(1) unsigned DEFAULT NULL,
+  `equip_locations` mediumint(7) unsigned DEFAULT NULL,
+  `weapon_level` tinyint(1) unsigned DEFAULT NULL,
+  `equip_level` tinyint(3) unsigned DEFAULT NULL,
+  `refineable` tinyint(1) unsigned DEFAULT NULL,
+  `view` smallint(5) unsigned DEFAULT NULL,
+  `script` text,
+  `equip_script` text,
+  `unequip_script` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UniqueAegisName` (`name_english`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
